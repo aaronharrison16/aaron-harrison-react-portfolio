d47b3a08ed4f89523be6d700bc83041cadc03187
Aaron Harrison <aaronharrison16@gmail.com>
2019-08-09 16:02:07 -0600

    practiced setting state

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -6,23 +6,41 @@ export default class PortfolioContainer extends Component {
   constructor() {
     super();
 
-    console.log("portfolio container has been rendered");
+    this.state = {
+      pageTitle: "Aaron Harrison React Portfolio",
+      data: [
+        {title: "Sprint"}, 
+        {title: "Boostability"},
+        {title: "SC Builders"}
+      ]
+    };
+
+    this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this);
   }
 
   portfolioItems() {
-    const data = ["Sprint", "Boostability", "SC Builders"];
-
-    return data.map(item => {
-      return <PortfolioItem title={item} />;
+    return this.state.data.map(item => {
+      return <PortfolioItem title={ item.title } />;
+    })
+  }
+  
+  handlePageTitleUpdate() {
+    this.setState({
+      pageTitle: "Something Else"
     })
   }
 
   render() {
     return (
       <div>
+        <h1>{ this.state.pageTitle }</h1>
         <h2>Portfolio Items go here...</h2>
         
         {this.portfolioItems()}
+
+        <hr />
+
+        <button onClick={this.handlePageTitleUpdate}>Change Title</button>
       </div>
     )
   }
