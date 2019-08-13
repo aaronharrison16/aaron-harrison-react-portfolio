bd63039e79ab32219031b291f5faf95a18171830
Aaron Harrison <aaronharrison16@gmail.com>
2019-08-12 22:05:44 -0600

    created initail app pages and added routing

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
 
 import PortfolioContainer from './portfolio/portfolio-container';
 import NavigationContainer from './navigation/navigation-container';
+import Home from './pages/home';
+import About from './pages/about';
+import Contact from './pages/contact';
+import Blog from './pages/blog';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        < NavigationContainer />
+        <Router>
+          <div>
+            < NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path='/about' component={About} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/blog' component={Blog} />
+            </Switch>
+          </div>
+        </Router>
+
         <h1>Aaron Harrison Portfolio</h1>
         <div>The day started { moment().startOf('day').fromNow() }</div>
         <PortfolioContainer />
--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { NavLink } from 'react-router-dom';
 
 export default class NavigationComponent extends Component {
   constructor() {
@@ -6,15 +7,13 @@ export default class NavigationComponent extends Component {
   }
   
 
-
   render() {
     return (
       <div>
-        <button>Home</button>
-        <button>About</button>
-        <button>Contact</button>
-        <button>Blog</button>
-        { true ? <button>Add Blog</button> : null }
+        <NavLink exact to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact" >Contact</NavLink>
+        <NavLink to="/blog" >Blog</NavLink>                   
       </div>
     )
   }
