b2cb77f17da155c26ed1b2bdf0a59d7183726451
Aaron Harrison <aaronharrison16@gmail.com>
2019-08-24 21:52:11 -0600

    connected email and forms to state

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
 
 export default class Login extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    console.log('handle submit', event)
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
   render() {
     return(
       <div>
         <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
         
-        <form>
-          <input type='text' />
-          <input type='password' />
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type='email'
+            name="email"
+            placeholder="Email"
+            value={this.state.email} 
+            onChange={this.handleChange}
+          />
+          <input 
+            type='password'
+            type='password'
+            name="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+
+          <div>
+            <button type="submit">Login</button>
+          </div>
         </form>
       </div>
     )
