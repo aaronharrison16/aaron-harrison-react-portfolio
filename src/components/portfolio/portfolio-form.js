33ac768621743633f4f0b3b77413eb9c66e63fc8
Aaron Harrison <aaronharrison16@gmail.com>
2019-09-08 11:57:02 -0600

    created form mixin and applied them to form styles

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -127,10 +127,7 @@ export default class PortfolioForm extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Portfolio Form</h1>
-
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} className="portfolio-form-wrapper">
           <div>
             <input
               type="text"
@@ -206,7 +203,6 @@ export default class PortfolioForm extends Component {
             <button type="submit">Save</button>
           </div>
         </form>
-      </div>
     );
   }
 }
