18e77b1fbfda0232cc577bdbfa99af2d5fa4a43a
Aaron Harrison <aaronharrison16@gmail.com>
2019-10-19 15:04:20 -0600

    removed portfolio item filters

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -18,56 +18,35 @@ export default class PortfolioContainer extends Component {
 
   getPortfolioItems() {
     axios
-      .get('https://aaronharrison.devcamp.space/portfolio/portfolio_items')
+      .get("https://aaronharrison.devcamp.space/portfolio/portfolio_items")
       .then(response => {
         // handle success
         this.setState({
           data: response.data.portfolio_items
-        })
+        });
       })
       .catch(error => {
         // handle error
         console.log(error);
-      })
-  }
-
-  handleFilter(filter) {
-    this.setState({
-      data: this.state.data.filter(item => {
-        return item.catagory === filter;
-      })
-    });
+      });
   }
 
   portfolioItems() {
-    // Data that we'll need:
-    // -background image: thumb_image_url
-    // -logo: logo_url
-    // -description: description
-    // -id: id
     return this.state.data.map(item => {
       return <PortfolioItem key={item.id} item={item} />;
-    })
+    });
   }
 
   componentDidMount() {
     this.getPortfolioItems();
   }
 
-  render() {    
+  render() {
     if (this.state.isLoading) {
-      return (
-        <div>Laoding...</div>
-      )
+      return <div>Laoding...</div>;
     }
     return (
-      <div className="portfolio-items-wrapper">
-        <button className='btn' onClick={() => this.handleFilter("Technology")} >Technology</button>
-        <button className='btn' onClick={() => this.handleFilter("Scheduling")} >Scheduling</button>
-        <button className='btn' onClick={() => this.handleFilter("Social Media")} >Social Media</button>
-
-        {this.portfolioItems()}
-      </div>  
-    )
+      <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
+    );
   }
 }
